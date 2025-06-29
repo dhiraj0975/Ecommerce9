0bf2ccec17db66bda7bb8c193f4372cd809c6d3a
Dhiraj Khobragade <brodj73@gmail.com>
2025-06-29 10:26:39 +0530

    deploy url changed than push frontend

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://ecommerce11-tjd2.vercel.app/api',
     withCredentials: true, // ⬅️ yeh add karo
 });
 
